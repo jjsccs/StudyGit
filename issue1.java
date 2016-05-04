e8d385e6d1c1da92e2352b830ecc71eae4686ab9
jsjang <jjsccs@gmail.com>
2016-05-04 22:55:09 +0900

    no message

--- issue1.java
+++ issue1.java
@@ -1,8 +1,1 @@
-Currently light is turn off by issue1..
-Now currently, light turn off by main...
-
-Branch issue1
-Branch issue1-1
-New Branch issue1-3
-Branch issue1-2
-New Branch issue1-1
+delete all contents. by local master

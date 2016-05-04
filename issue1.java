d37be6d93eec2ff728a73a1e99a7a541495904ba
jsjang <jjsccs@gmail.com>
2016-05-05 00:33:43 +0900

    no message

--- issue1.java
+++ issue1.java
@@ -1,8 +1,4 @@
-Currently light is turn off by issue1..
-Now currently, light turn off by main...
 
-Branch issue1
-Branch issue1-1
-New Branch issue1-3
-Branch issue1-2
-New Branch issue1-1
+
+This is issue1 branch... 2016.05.05
+Today is kid's day..
